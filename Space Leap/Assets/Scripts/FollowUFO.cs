5b05381defa64095e4bc83ae82402be088229554
Justin Lafortune <justinlafor@gmail.com>
2018-05-27 23:08:03 -0400

    UFO symbol for distance to user. Still not complete

---
+++ Space Leap/Assets/Scripts/FollowUFO.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowUFO : MonoBehaviour
+{
+
+    public GameObject ufo;       //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = new Vector3(ufo.transform.position.x, transform.position.y, 0);
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        offset = new Vector3(ufo.transform.position.x, transform.position.y, 0);
+
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = offset;
+    }
+}
