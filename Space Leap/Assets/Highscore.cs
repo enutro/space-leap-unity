64fe74aa5776e828c071f58f6f93ac32c44d7d09
Justin Lafortune <justinlafor@gmail.com>
2018-06-02 02:47:10 -0400

    highscore local

---
+++ Space Leap/Assets/Highscore.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highscore : MonoBehaviour {
+    public static int score;
+
+    public static int highscore;
+    public Transform player;
+    public int numOfLazersHopped;
+    public float yDistanceBetweenLaser;
+    public float yStartLaser;
+
+    TextMesh text;
+
+    void Start()
+    {
+        text = GetComponent<TextMesh>();
+        numOfLazersHopped = 0;
+        score = 0;
+
+        highscore = PlayerPrefs.GetInt("highscore", highscore);
+    }
+
+    void Update()
+    {
+        if (player.transform.position.y > (numOfLazersHopped * yDistanceBetweenLaser) + yStartLaser)
+        {
+            numOfLazersHopped++;
+        }
+
+
+        text.text = "" + numOfLazersHopped + " highscore:" + highscore;
+
+
+        if (numOfLazersHopped > highscore)
+        {
+            highscore = numOfLazersHopped;
+            text.text = "" + numOfLazersHopped;
+
+            PlayerPrefs.SetInt("highscore", highscore);
+        }
+    }
+
+    public static void AddPoints(int pointsToAdd)
+    {
+        score += pointsToAdd;
+    }
+
+    public static void Reset()
+    {
+        score = 0;
+    }
+}
---
+++ Space Leap/Assets/localHighscore.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class localHighscore : MonoBehaviour
+{
+
+    public static int score;
+
+    public static int highscore;
+    public Transform player;
+    public int numOfLazersHopped;
+    public float yDistanceBetweenLaser;
+    public float yStartLaser;
+
+    TextMesh text;
+
+    void Start()
+    {
+        text = GetComponent<TextMesh>();
+        numOfLazersHopped = 0;
+        score = 0;
+
+        highscore = PlayerPrefs.GetInt("highscore", highscore);
+    }
+
+    void Update()
+    {
+        if (player.transform.position.y > (numOfLazersHopped * yDistanceBetweenLaser) + yStartLaser)
+        {
+            numOfLazersHopped++;
+        }
+        if (numOfLazersHopped > highscore)
+        {
+            highscore = numOfLazersHopped;
+            text.text = "" + numOfLazersHopped;
+
+            PlayerPrefs.SetInt("highscore", highscore);
+        }
+    }
+
+    public static void AddPoints(int pointsToAdd)
+    {
+        score += pointsToAdd;
+    }
+
+    public static void Reset()
+    {
+        score = 0;
+    }
+}
