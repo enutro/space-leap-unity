514b2e8668d606edc46ed70b49e0330514a893fa
Justin Lafortune <justinlafor@gmail.com>
2018-06-03 11:05:47 -0400

    All new assets(unorganized)

---
+++ Space Leap/Assets/LaunchTower.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchTower : MonoBehaviour
+{
+
+
+
+    public float speed;
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    public float foreGroundSpeedToBackgroundSpeedRatio = 20;
+    // Use this for initialization
+    void Start()
+    {
+        speed = 0.01f;
+
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+    }
+
+    void Update()
+    {
+    
+            if ( transform.position.y < 5.1f)
+            {
+                speed = speed + 0.0015f;
+                offset = new Vector3(0, transform.position.y + speed, 0);
+
+            }
+            else if (transform.position.y < 22.4f)
+        {
+            speed = speed + 0.005f;
+            offset = new Vector3(0, transform.position.y + speed, 0);
+        }
+            else
+            {
+                speed = 0;
+                offset = new Vector3(0, 22.4f, 0);
+
+            }
+
+            transform.position = offset;
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+
+      
+    }
+}
